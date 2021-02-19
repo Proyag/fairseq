db1e88298e3075fdad96ba043b120524681954aa
Proyag <proyag.pal@ed.ac.uk>
2021-02-19 13:55:15 +0000

    Add option to check consistency of masked model

--- scripts/mask_linears.py
+++ scripts/mask_linears.py
@@ -11,6 +11,8 @@ def get_parser():
     # fmt: off
     parser.add_argument('-i', '--checkpoint-in', required=True, help='checkpoint file to read')
     parser.add_argument('-o', '--checkpoint-out', required=True, help='checkpoint file to write to')
+    parser.add_argument('-c', '--check-original', required=False, help='pre-finetuning checkpoint to verify frozen parameters')
+    parser.add_argument('-C', '--check-only', required=False, action='store_true', help='only check')
     # fmt: on
 
     return parser
@@ -22,13 +24,22 @@ def main():
 
     checkpoint = torch.load(args.checkpoint_in)
 
+    if args.check_original:
+        orig_model = torch.load(args.check_original)['model']
+
     # Set this config entry to False to disable masking during decoding with this checkpoint
     # since the mask is already applied here
     checkpoint['cfg'].masking.masked_finetune = False
     threshold = checkpoint['cfg'].masking.masked_finetune_threshold
 
     for k in list(checkpoint['model'].keys()):
-        if k.endswith('.mask'):
+        if args.check_original and not k.endswith('.mask'):
+            if torch.eq(checkpoint['model'][k], orig_model[k]).all():
+                print("Equal: {}".format(k))
+            else:
+                print("NOT equal {}".format(k))
+
+        elif k.endswith('.mask') and not args.check_only:
             layer_name = '.'.join(k.split('.')[:-1])
             weight = layer_name + '.weight'
             mask = k
@@ -42,7 +53,8 @@ def main():
             # Remove mask from checkpoint
             del checkpoint['model'][mask]
 
-    torch.save(checkpoint, args.checkpoint_out)
+    if not args.check_only:
+        torch.save(checkpoint, args.checkpoint_out)
 
 
 if __name__ == "__main__":
